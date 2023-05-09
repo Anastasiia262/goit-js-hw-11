53f8bed71d3af33d9285ec6f5f66a75c6f0d260b
Anastasiia262 <gribskay2@gmail.com>
2023-05-09 19:06:32 +0300

    fix

--- src/js/search-picture.js
+++ src/js/search-picture.js
@@ -6,15 +6,14 @@ export default class ImagesApiServer {
     this.page = 0;
     this.pageSize = 40;
   }
+
   async fetchImage() {
-    this.incrementPage();
+    const currentPage = this.page + 1;
     const BASE_URL = 'https://pixabay.com/api/';
     const API_KEY = '28875858-be00e402ceba03ed1852ded5b';
     const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
       this.searchQuery
-    )}&image_type=photo&orientation=horizontal&safesearch=true&page=${
-      this.page
-    }&per_page=${this.pageSize}`;
+    )}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=${this.pageSize}`;
 
     try {
       const images = await axios.get(url);
@@ -23,6 +22,7 @@ export default class ImagesApiServer {
       throw new Error('Sorry, something goes wrong.');
     }
   }
+
   incrementPage() {
     this.page += 1;
   }
@@ -30,6 +30,7 @@ export default class ImagesApiServer {
   resetPage() {
     this.page = 0;
   }
+
   get query() {
     return this.searchQuery;
   }
